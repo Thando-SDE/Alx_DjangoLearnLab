d224f85461f9042be4560f29d54c052173cb7870
Thando-SDE <ThandoMjacu@gmail.com>
2025-11-17 21:32:35 +0200

    Fix: Simplify views and ensure clear DetailView usage

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Library
 from .models import Book
+from .models import Library
 
-# Function-based view to list all books
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
@@ -12,9 +12,3 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        library = self.get_object()
-        context['books'] = library.books.all()
-        return context
