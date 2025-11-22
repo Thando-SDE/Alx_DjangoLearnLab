5e82f49a47de10d5dd875582822477edea87e967
Thando-SDE <ThandoMjacu@gmail.com>
2025-11-22 15:46:45 +0200

    Fix: Use ListView instead of DetailView

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-class LibraryBooksView(DetailView):
+class LibraryBooksView(ListView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
