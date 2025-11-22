80d13110aef2595698cef4a6893f704e2cfd55dd
Thando-SDE <ThandoMjacu@gmail.com>
2025-11-22 15:54:01 +0200

    Fix: Use exact ALX template format with library.books.all and LibraryDetailView

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-class LibraryBooksView(ListView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
