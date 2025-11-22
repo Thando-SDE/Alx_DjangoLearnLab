15efeb353d41dbac565ac3fb0083f55936483e1b
Thando-SDE <ThandoMjacu@gmail.com>
2025-11-22 16:20:08 +0200

    Fix: Use LibraryBooksView class name with ListView

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,7 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-class LibraryDetailView(ListView):
+class LibraryBooksView(ListView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
