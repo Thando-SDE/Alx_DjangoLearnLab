120f9a331eeec7de0deb50fc6b6dd051e85d323b
Thando-SDE <ThandoMjacu@gmail.com>
2025-11-23 17:21:17 +0200

    Add all required security settings to settings.py
    - DEBUG = False
    - SECURE_BROWSER_XSS_FILTER = True
    - X_FRAME_OPTIONS = 'DENY'
    - SECURE_CONTENT_TYPE_NOSNIFF = True
    - CSRF_COOKIE_SECURE = True
    - SESSION_COOKIE_SECURE = True

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,7 +1,6 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
+# Security Settings
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
