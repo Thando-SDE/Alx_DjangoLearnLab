753010a6b648a3c89a279ce94c143a24b61dbd02
Thando-SDE <ThandoMjacu@gmail.com>
2025-11-23 14:45:14 +0200

    Fix: Move Book model to bookshelf app as required

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required, login_required
+from .models import Book
 
-# Create your views here.
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return HttpResponse(f"Book list view - {books.count()} books")
+
+@login_required  
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    return HttpResponse("Book create view - permission granted")
+
+@login_required
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, book_id):
+    return HttpResponse(f"Book edit view - editing book {book_id}")
+
+@login_required
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, book_id):
+    return HttpResponse(f"Book delete view - deleting book {book_id}")
