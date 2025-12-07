f48b1ca0b7f98f0164e02c9b6ec59c8e86642964
Thando-SDE <ThandoMjacu@gmail.com>
2025-12-07 23:06:20 +0200

    Task 3: Final fix - URL pattern post/<int:pk>/comments/new/ implemented and working

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -104,7 +104,7 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/comment_form.html'
     
     def form_valid(self, form):
-        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
         form.instance.post = post
         form.instance.author = self.request.user
         return super().form_valid(form)
